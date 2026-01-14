21dbd050b06ce3dd7dbfd13b9c34e1cd86c27eca
Seiichi <ar.seiichi.1029@gmail.com>
2026-01-14 22:54:10 +0900

    feat: add reddit hot source

---
+++ docs/reddit.js
@@ -0,0 +1,11 @@
+﻿fetch("https://www.reddit.com/r/all/hot.json?limit=20")
+  .then(r => r.json())
+  .then(d => {
+    const items = d.data.children.map((c,i)=>({
+      rank:i+1,
+      title:c.data.title,
+      score:c.data.score,
+      url:"https://reddit.com"+c.data.permalink
+    }));
+    console.log(items);
+  });
